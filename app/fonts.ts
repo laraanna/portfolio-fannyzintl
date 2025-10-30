be2ea16acd57e8a54894ed57ce94eb1d8d36cae2
Lara <lara@laraannawagner.com>
2025-10-30 16:38:51 +0100

    revert fontpath

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const iowan = localFont({
   src: [
     {
-      path: "../fonts/iowan-old-style.woff",
+      path: "../public/fonts/iowan-old-style.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/iowan-old-style-italic.woff",
+      path: "../public/fonts/iowan-old-style-italic.woff",
       weight: "400",
       style: "italic",
     },
